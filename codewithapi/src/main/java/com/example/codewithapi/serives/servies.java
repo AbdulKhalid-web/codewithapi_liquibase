ac8073bb527029a75a2be63c217e15db95c74bad
MrKhalid <abkhalid291@gmail.com>
2024-04-26 17:31:51 +0530

    Revert "Revert "best version thats the""
    This reverts commit 787db79f

--- codewithapi/src/main/java/com/example/codewithapi/serives/servies.java
+++ codewithapi/src/main/java/com/example/codewithapi/serives/servies.java
@@ -2,19 +2,27 @@ package com.example.codewithapi.serives;
 
 import com.example.codewithapi.dao.ProductRepository;
 import com.example.codewithapi.product.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class servies {
+    @Autowired
     private ProductRepository productRepository;
 
     public List<Product> listall(){
         return productRepository.findAll();
     }
-    public Product getProduct(int id){
-        return productRepository.getReferenceById(id);
+    public Product getProduct(int id) {
+//        productRepository.findAllById(id);
+        Optional<Product> productOptional = productRepository.findById(id);
+        return productOptional.orElse(null);
+
     }
+
     public void save(Product product) {
         productRepository.save(product);
     }
@@ -22,5 +30,17 @@ public class servies {
     public void deletepro(int id){
         productRepository.deleteById(id);
     }
+    public String update(int id, Product updatedProduct) {
+        Optional<Product> optionalProduct = productRepository.findById(id);
+        if (optionalProduct.isPresent()) {
+            Product product = optionalProduct.get();
+            product.setName(updatedProduct.getName());
+            product.setModel(updatedProduct.getModel());
+            product.setDescription(updatedProduct.getDescription());
+            productRepository.save(product);
+            return "Product updated successfully";
+        } else {
+            return "Product not found";
+        }}
 
-}
+    }
